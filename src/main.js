657bf8395b5f0bd1beda6833507ff150e60745a9
younoah <kr_uno_10@naver.com>
2021-02-04 18:59:05 +0900

    [feat] add initGame, startGame, showTimerAmdScore, startTimer

--- src/main.js
+++ src/main.js
@@ -1,3 +1,87 @@
 'use strict';
 
-console.log('hello');
+const CARROT_SIZE = 80;
+const CARROT_COUNT = 5;
+const BUG_COUNT = 5;
+const GAME_DURATION = 5;
+
+const field = document.querySelector('.game__field');
+const fieldRect = field.getBoundingClientRect();
+const gameBtn = document.querySelector('.game__button');
+const gameTimer = document.querySelector('.game__timer');
+const gameScore = document.querySelector('.game__score');
+const popUp = document.querySelector('.pop-up');
+
+let started = false;
+let score = 0;
+let timer = undefined;
+
+gameBtn.addEventListener('click', startGame);
+
+function startGame() {
+  started = true;
+  initGame();
+  showStopButton();
+  showTimerAndScore();
+  startGameTimer();
+}
+
+function showStopButton() {
+  const icon = gameBtn.querySelector('.fas');
+  icon.classList.add('fa-pause');
+  icon.classList.remove('fa-play');
+}
+
+function showTimerAndScore() {
+  gameTimer.style.visibility = 'visible';
+  gameScore.style.visibility = 'visible';
+}
+
+function startGameTimer() {
+  let remainingTimeSec = GAME_DURATION;
+  updateTimerText(remainingTimeSec);
+  timer = setInterval(() => {
+    if (remainingTimeSec <= 0) {
+      clearInterval(timer);
+      return;
+    }
+    updateTimerText(--remainingTimeSec);
+  }, 1000);
+}
+
+function updateTimerText(time) {
+  const minutes = Math.floor(time / 60);
+  const sconds = time % 60;
+  gameTimer.innerText = `${minutes} : ${sconds}`;
+}
+
+function stopGame() {
+  started = false;
+}
+
+function initGame() {
+  field.innerHTML = '';
+  addItem('carrot', CARROT_COUNT, 'img/carrot.png');
+  addItem('bug', BUG_COUNT, 'img/bug.png');
+}
+
+function addItem(className, count, imgPath) {
+  const x1 = 0;
+  const y1 = 0;
+  const x2 = fieldRect.width - CARROT_SIZE;
+  const y2 = fieldRect.height - CARROT_SIZE;
+  for (let i = 0; i < count; i++) {
+    const item = document.createElement('img');
+    item.setAttribute('class', className);
+    item.setAttribute('src', imgPath);
+    item.style.position = 'absolute';
+    const x = randomNumber(x1, x2);
+    const y = randomNumber(y1, y2);
+    item.style.left = `${x}px`;
+    item.style.top = `${y}px`;
+    field.append(item);
+  }
+}
+function randomNumber(min, max) {
+  return Math.random() * (max - min) + min;
+}
