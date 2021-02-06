9639a9e902e42674df21f3c8053677aff9375fef
younoah <kr_uno_10@naver.com>
2021-02-06 15:55:18 +0900

    [refactor] add sound.js for gameSound

--- src/sound.js
+++ src/sound.js
@@ -1,1 +1,40 @@
 'use strict';
+
+const carrotSound = new Audio('./sound/carrot_pull.mp3');
+const bugSound = new Audio('./sound/bug_pull.mp3');
+const alertSound = new Audio('./sound/alert.wav');
+const winSound = new Audio('./sound/game_win.mp3');
+const bgSound = new Audio('./sound/bg.mp3');
+
+export function playCarrot() {
+  playSound(carrotSound);
+}
+
+export function playBug() {
+  playSound(bugSound);
+}
+
+export function playAlert() {
+  playSound(alertSound);
+}
+
+export function playWin() {
+  playSound(winSound);
+}
+
+export function playBackground() {
+  playSound(bgSound);
+}
+
+export function stopBackground() {
+  stopSound(bgSound);
+}
+
+function playSound(sound) {
+  sound.currentTime = 0;
+  sound.play();
+}
+
+function stopSound(sound) {
+  sound.pause();
+}
